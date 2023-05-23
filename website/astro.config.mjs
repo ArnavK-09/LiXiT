0b9cf17bed70ad1acbddbf162f6b8480b6a9ea7a
ArnavK-09 <arnavkaushal09@gmail.com>
2023-05-23 03:49:00 +0000

    Initial commit

---
+++ website/astro.config.mjs
@@ -0,0 +1,11 @@
+// imports
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://lixit.space/",
+  integrations: [tailwind(), sitemap(), alpinejs()],
+});
